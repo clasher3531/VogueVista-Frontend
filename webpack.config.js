eec70565b8830bdf9665e24fa3d98379cf6c2d60
clasher3531 <nikhil.a.joshi@capgemini.com>
2025-07-10 10:26:28 +0530

    Fix asset paths in index.html and set publicPath in webpack config for correct resource loading

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = (env, argv) => {
     output: {
       path: path.resolve(__dirname, "dist"),
       filename: "bundle.[contenthash].js",
+      publicPath: "/",
       clean: true,
     },
     module: {
